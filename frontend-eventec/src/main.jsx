f01ec5d1b3e26df818e8292a827e26215465b07d
Todd <luiscatodd@gmail.com>
2023-09-07 20:48:08 -0600

    Bootstrap

--- frontend-eventec/src/main.jsx
+++ frontend-eventec/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import Root from './routes/root'
 
