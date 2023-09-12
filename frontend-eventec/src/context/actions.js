c0064e5339bee12dd5978fd8933f84f9c4a27bfe
Todd <luiscatodd@gmail.com>
2023-09-12 17:10:49 -0600

    funciones registrar

--- frontend-eventec/src/context/actions.js
+++ frontend-eventec/src/context/actions.js
@@ -51,6 +51,58 @@ export async function loginEstudiante(dispatch, loginPayload) {
     dispatch({ type: 'LOGIN_ERROR', error: error });
   }
 }
+
+export async function registerEstudiante(dispatch, registerEstudiantePayload){
+  const requestOptions = {
+    method: 'POST',
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(registerEstudiantePayload),
+  };
+ 
+  try {
+    dispatch({ type: 'REQUEST_LOGIN' });
+    let response = await fetch(`${ROOT_URL}/register/estudiante`, requestOptions);
+    let data = await response.json();
+ 
+    if (data.user) {
+      dispatch({ type: 'LOGIN_SUCCESS', payload: data });
+      localStorage.setItem('currentUser', JSON.stringify(data));
+      return data
+    }
+ 
+    dispatch({ type: 'LOGIN_ERROR', error: data.errors[0] });
+    return;
+  } catch (error) {
+    dispatch({ type: 'LOGIN_ERROR', error: error });
+  }
+}
+
+export async function registerAso(dispatch, registerAsoPayload){
+  const requestOptions = {
+    method: 'POST',
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(registerAsoPayload),
+  };
+ 
+  try {
+    dispatch({ type: 'REQUEST_LOGIN' });
+    let response = await fetch(`${ROOT_URL}/register/aso`, requestOptions);
+    let data = await response.json();
+ 
+    if (data.user) {
+      dispatch({ type: 'LOGIN_SUCCESS', payload: data });
+      localStorage.setItem('currentUser', JSON.stringify(data));
+      return data
+    }
+ 
+    dispatch({ type: 'LOGIN_ERROR', error: data.errors[0] });
+    return;
+  } catch (error) {
+    dispatch({ type: 'LOGIN_ERROR', error: error });
+  }
+}
  
 export async function logout(dispatch) {
   dispatch({ type: 'LOGOUT' });
