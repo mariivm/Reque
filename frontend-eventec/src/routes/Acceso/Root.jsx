eaaa92a5281e9369e0864ec0a7bb6cef51a11681
Victoria Sandí <vsandi@estudiantec.cr>
2023-09-12 14:46:37 -0600

    Correcciones y creacion de carpetas (pantallas)

--- frontend-eventec/src/routes/Acceso/Root.jsx
+++ frontend-eventec/src/routes/Acceso/Root.jsx
@@ -1,11 +1,12 @@
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import Checkbox from '../assets/checkbox';
+import Checkbox from '../../assets/checkbox';
 import { Button } from 'react-bootstrap';
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from 'react';
-import { loginAso, useAuthDispatch } from '../context';
+import { loginAso, useAuthDispatch } from '../../context';
+import styles from "./acceso.module.css";
 
 const Root = () => {
   const [correoAsociacion, setCorreoAsociacion] = useState("");
@@ -28,44 +29,44 @@ const Root = () => {
   }
   return (
     <div>
-      <span style={{ left: "20px", top: "10px" }}>EVEN</span>
-      <span style={{ color: "#22AAA1", left: "20px", top: "10px" }}>TEC</span>
+      <span className={styles.span} style={{ color: "#FFFFFF", left: "20px", top: "10px" }}>EVEN</span>
+      <span className={styles.span} style={{ left: "20px", top: "10px" }}>TEC</span>
 
-      <h1 style={{ color: "#FFFFFF", textAlign: "center", top: "40px" }}>
+      <h1 className={styles.h1}>
         Iniciar Sesión
       </h1>
 
-      <p style={{ textAlign: "center", color: "#22AAA1", top: "50px" }}>
+      <p className={styles.p}>
         Ingresa a la plataforma como asociación
       </p>
 
-      <div class="cuadrado" style={{ margin: "auto", textAlign: "center", top: "80px" }}>
+      <div className={styles.cuadrado}>
         <Row>
-          <Col xs={10} style={{ position: "relative", margin: "auto", top: "30px" }}>
+          <Col xs={10} className={styles.col}>
             <Form.Control value={correoAsociacion} onChange={e => setCorreoAsociacion(e.target.value)} type="text" placeholder={"Correo de Asociación"} style={{ textAlign: "center" }}/>
             </Col>
         </Row>
         <br/>
         <Row>
-          <Col xs={10} style={{ position: "relative", margin: "auto", top: "30px" }}>
+          <Col xs={10} className={styles.col}>
             <Form.Control value={contrasena} onChange={e => setContrasena(e.target.value)} type="text" placeholder={"Contraseña"} style={{ textAlign: "center" }}/>
             </Col>
         </Row>
-        <br/>
-        <br/>
+      <br/>
+      <br/>
 
-        <Checkbox/>
+      <Checkbox/>
 
-        <Button style={{ backgroundColor: "#22AAA1", width: "380px", height: "50px" }} onClick={handleLogin}> Ingresar </Button>
-        <br/>
-        <br/>
+      <Button className={styles.button} onClick={handleLogin}> Ingresar </Button>
+      <br/>
+      <br/>
 
-        <span style={{ color: "gray"}}>¿No tienes una cuenta?</span>
-        <span> <Link to="/registroAso" style={{ color: "#22AAA1" }}> <u>Regístrate</u></Link> </span>
+      <span className={styles.span} style={{ color: "gray"}}>¿No tienes una cuenta?</span>
+      <span className={styles.span}> <Link to="/registroAso"> <u>Regístrate</u></Link> </span>
 
-        <br/>
+      <br/>
 
-        <span> <Link to="/loginEstudiante" style={{color: "#22AAA1"}}><u>Iniciar sesión como estudiante</u></Link> </span>
+      <span className={styles.span}> <Link to="/loginEstudiante"><u>Iniciar sesión como estudiante</u></Link> </span>
       </div>
     </div>
   )
