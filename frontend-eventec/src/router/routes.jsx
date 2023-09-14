93947763155e2635d7108e139d841047b6a629fe
Victoria Sandí <vsandi@estudiantec.cr>
2023-09-14 00:02:49 -0600

    Avance colaboradores

--- frontend-eventec/src/router/routes.jsx
+++ frontend-eventec/src/router/routes.jsx
@@ -9,12 +9,10 @@ import Feedback from '../routes/Feedback/Feedback';
 import Foro from '../routes/Foro/Foro';
 import FormActividad from '../routes/FormActividad/FormActividad';
 import FormPropuesta from '../routes/FormPropuesta/FormPropuesta';
-<<<<<<< HEAD
 import Colaboradores from '../routes/Colaboradores/Colaboradores';
-=======
+import AgregarColaborador from '../routes/Colaboradores/AgregarColaborador';
 import Propuestas from '../routes/Propuestas/Propuestas'
 // import FormEvento from '../routes/FormEvento/FormEvento';
->>>>>>> a50b2daa60822d71700cd94133823a06dfd935d4
 
 const routes = 
 [
@@ -74,15 +72,19 @@ const routes =
       isPrivate: true,
     },
     {
-<<<<<<< HEAD
       path: "/colaboradores",
       element: <Colaboradores />,
       isPrivate: true,
-=======
+    },
+    {
+      path: "/agregarColabs",
+      element: <AgregarColaborador />,
+      isPrivate: false,
+    },
+    {
       path: "/verPropuestas",
       element: <Propuestas />,
       isPrivate: true
->>>>>>> a50b2daa60822d71700cd94133823a06dfd935d4
-    }
+    },
 ]
 export default routes
---
+++ frontend-eventec/src/routes/Colaboradores/AgregarColaborador.jsx
@@ -0,0 +1,56 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import Radio from '../../components/Radio/Radio'
+import styles from "../Feedback/feedback.module.css";
+
+const AgregarColaborador = () => {
+  return (
+    <div>
+      <span className={styles.span} style={{ color: "#FFFFFF", left: "20px", top: "10px" }}>EVEN</span>
+      <span className={styles.span} style={{ left: "20px", top: "10px" }}>TEC</span>
+
+      <h1 className={styles.h1}>
+        Enviar Feedback
+      </h1>
+
+      <p className={styles.p}>
+        Comentarios y recomedaciones sobre nuestros eventos
+      </p>
+
+      <Form>
+        <fieldset>
+          <Form.Group className={styles.select}>
+            <Form.Label htmlFor="Select">Evento</Form.Label>
+            <Form.Select id="Select">
+              <option></option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className={styles.input}>
+            <Form.Label htmlFor="TextInput">Comentario</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder=""
+              style={{ height: '300px' }}
+            />
+          </Form.Group>
+          <Button className={styles.button} type="submit">Enviar</Button>
+        </fieldset>
+      </Form>
+      <Row className={styles.radio}>
+        <Col>
+          <Radio nombreCalificacion={"Calificación del lugar"}/>
+        </Col>
+        <Col>
+          <Radio nombreCalificacion={"Calificación del horario"}/>
+        </Col>
+        <Col>
+          <Radio nombreCalificacion={"Calificación de las actividades"}/>
+        </Col>
+        <Col>
+          <Radio nombreCalificacion={"Calificación de la organización"}/>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default AgregarColaborador;
