732018ea012bf2a30bab2e396f17e7d55abbda6c
Todd <luiscatodd@gmail.com>
2023-09-13 18:28:48 -0600

    Solucionar mas conflictos de merge

--- backend-eventec/db_functions/Usuarios.py
+++ backend-eventec/db_functions/Usuarios.py
@@ -16,13 +16,8 @@ def SP_insertarAso(correo, nombre, contrasena):
     cursor = conn.cursor()
     query = """\
         SET NOCOUNT ON;
-<<<<<<< HEAD
         DECLARE @RC  int;
         EXEC @RC = [eventec].[dbo].[Reg_Aso] ?, ?, ?
-=======
-        DECLARE @RC int;
-        EXEC @RC = [dbo]. ?, ?, ?
->>>>>>> 620435f4453c6f3bdef4420f8f2654b512eb7ccc
         SELECT @RC AS rc;
     """
     cursor.execute(query, (correo, nombre, contrasena))
@@ -33,20 +28,10 @@ def SP_insertarAso(correo, nombre, contrasena):
 def fetchUsuarioEstudiante(correo, contrasena):
     conn = Connection().db
     cursor = conn.cursor()
-<<<<<<< HEAD
     query = """\
         EXEC [eventec].[dbo].[Login_Estu] ?, ?
         """
     cursor.execute(query,(correo,contrasena))
-=======
-    query = query = """\
-        SET NOCOUNT ON;
-        DECLARE @RC int;
-        EXEC @RC = [my_database].[dbo].[my_sp] ?, ?, ?, ?
-        SELECT @RC AS rc;
-    """
-    cursor.execute(query)
->>>>>>> 620435f4453c6f3bdef4420f8f2654b512eb7ccc
     datos = cursor.fetchval()
     cursor.close()
     return datos
